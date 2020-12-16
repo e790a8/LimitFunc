e779837c6950b2d7b93eb509f179c1dc019403dd
lisztomania <lisztomania@vip.qq.com>
2020-12-16 20:22:36 +0800

    init

---
+++ LimitFunc/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Author   : lisztomania
+# @Date     : 2020/12/16 19:03
+# @Software : PyCharm
+# @Version  : Python 3.9.1
+# @Project  : LimitFunc
+# @FileName : __init__.py.py
+# @Function :
+from __future__ import absolute_import
+from .LimitFunc import *
+
+name = 'lisztomania'
